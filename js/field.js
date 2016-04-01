98206f5f980c335fd68cbd3dba4f724bbae4aeb4
broezer@gmail.com <broezer@gmail.com>
2016-04-01 10:00:28 +0200

    Update aSync with script.js
    https://github.com/ded/script.js

--- js/field.js
+++ js/field.js
@@ -8,13 +8,14 @@ if (!window.requestAnimationFrame) {
 }
 
 // Init Stats
+/*
 var stats = new Stats();
 stats.setMode(0);
 stats.domElement.style.position = 'absolute';
 stats.domElement.style.left = '0px';
 stats.domElement.style.top = '0px';
 document.body.appendChild(stats.domElement);
-
+*/
 
 /*!
  * Mantis.js / jQuery / Zepto.js plugin for Constellation
@@ -167,9 +168,9 @@ document.body.appendChild(stats.domElement);
 			callback();
 
 			window.requestAnimationFrame(function () {
-				stats.begin(); // Only for Stats
+				//stats.begin(); // Only for Stats
 				this.loop(callback);
-				stats.end(); // Only for Stats
+				//stats.end(); // Only for Stats
 			}.bind(this));
 		};
 
